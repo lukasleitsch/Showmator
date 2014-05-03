52f09e973eec2a7a5bb0e9c9b8e99c950c87533f
Lukas Leitsch <lukas@leitsch.org>
2014-05-03 15:35:48 +0200

    fixed merge conflicts

--- extension_chrome/showmator/js/popup.js
+++ extension_chrome/showmator/js/popup.js
@@ -1,88 +1,112 @@
-var socket = io.connect('http://localhost:63685');
+/*global chrome */
+/*global io */
 
-$(document).ready(function(){
-  var title;
-  var url;
+$(function() {
+  var title, url,
+      
+      socket = io.connect('http://localhost:63685'),
 
-  // Titel und URL des aktuellen Tabs auslesen
-  chrome.tabs.getSelected(null,function(tab) { 
-    title = tab.title;
-    url = tab.url;
+      $title       = $('#title'),
+      $saveChanges = $('#save-changes'),
+      $alertInfo   = $('.alert-info'),
 
-    $('#title').val(title);
+      htmlEntities = function(str) {
+        // TODO why `String()`?
+        return String(str)
+          .replace(/&/g, '&amp;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+          .replace(/"/g, '&quot;');
+      };
 
-    /* Breite des Popups anpassen */
 
-    var length = $('#title').val().length;
 
-    $('#title').attr("size", length*1.2);
+  // bind events
+  // -----------------------------------------------------------------------------
 
-    if ($(window).width() < 220) {
-      $('body').width(220);
-    }
+  // change title
+  $saveChanges.click(function() {
+    if ($(this).hasClass('disabled'))
+      return;
 
-     /* Böse URLs */
-
-    var badurl = localStorage['badUrls'].split('\n');
-    for (var i = 0; i < badurl.length; i++) {
-      console.log(url+" // "+ badurl);
-      if(url == badurl[i]){
-        $('#badUrl').html('<div class="alert alert-error">Böse URL: Kann nicht eingetragen werden!</div>');
-        $('#insert, #text, .text, #title, #dublicate, #delete').remove();  
-      }
-    };
-
-    // Dublicate überprüfen. Wird direkt ausgeführt beim öffnen des Popups
-
-    socket.emit('check_dublicate', {slug: localStorage['slug'], url: htmlEntities(url)});
-  });
-
-  // Button zum Einfügen
-
-  $( "#insert" ).click(function() {
-
-    title = htmlEntities($('#title').val());
-    if($('#text').is(":checked")){
+    title = htmlEntities($title.val());
+    if ($('#kind-text-only').is(":checked"))
       url = null;
-    }
-    console.log(title+" "+url);
-    socket.emit('add', {slug: localStorage['slug'], title: title, url: url});
+    // TODO make change event
+    socket.emit('add', {slug: localStorage.slug, title: title, url: url});
   });
 
-  // Behebt den Bug von Chrome, dass der Focus nicht direkt auf eine Element gesetzt werden kann. 
 
-  setTimeout(function() {
-    $('#insert').focus();
-  }, 100);
-
-  // Löschen Button
-
-  $('#delete').click(function(){
-    socket.emit('delete', {slug: localStorage['slug']});
+  // delete entry
+  $('remove-entry').click(function() {
+    // TODO works with just the slug? even with non-links?
+    socket.emit('delete', {slug: localStorage.slug});
   });
 
-  // Blendet die Meldung ein, wenn der Server meldet, dass der Link schon eingetragen wurde
 
-  socket.on('dublicate', function(){
-    $('#dublicate').html('<div class="alert alert-error">Dieser Link ist schon eingetragen!</div>');
+  // prevent duplication
+  socket.on('duplicate', function(){
+    $('#duplicate').html('<div class="alert alert-error">Dieser Link ist schon eingetragen!</div>');
     $('#insert').html("Trotzdem einfügen");
   });
 
-  // Ist der Link eingetragen, meldet der Server dies und das Popup wird geschlossen und das Badget wird gesetzt
 
+  // Ist der Link eingetragen, meldet der Server dies und das Popup wird geschlossen und das Badget wird gesetzt
   socket.on('close', function(){
-   chrome.extension.getBackgroundPage().badget("OK", "#33cc00");
+    chrome.extension.getBackgroundPage().badget("OK", "#33cc00");
     window.close();
   });
 
-  // Sonderzeichen escapen
 
-  function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-    // return str;
-  }
+  // react on enter/escape + hide alert when title is changed
+  $title.keyup(function(e) {
 
+    // on enter: save changes
+    if (e.keyCode == 13) {
+      $saveChanges.click();
+      return;
+
+    // on escape: close popup
+    } else if (e.keyCode == 27) {
+      window.close();
+      return;
+    }
+
+    // if title has changed: hide alert and enable save button
+    if ($alertInfo.is(':hidden') && htmlEntities($title.val()) != title) {
+      $alertInfo.slideUp();
+      $saveChanges.removeClass('disabled');
+    }
+  });
+
+  
+
+  // get tab title and url + send link to server
+  // -----------------------------------------------------------------------------
+  
+  chrome.tabs.getSelected(null, function(tab) {
+    title = htmlEntities(tab.title);
+    url   = tab.url;
+
+    $title.val(title);
+
+    // prevent if on blacklist
+    // TODO make forEach
+    // TODO control via body class
+    var blacklist = localStorage.blacklist.split('\n'); // TODO works on windows?
+    for (var i = 0; i < blacklist.length; i++) {
+      if (url == blacklist[i]) {
+        $('#badUrl').html('<div class="alert alert-error">Böse URL: Kann nicht eingetragen werden!</div>');
+        $('#insert, #text, .text, #title, #duplicate, #delete').remove();
+      }
+    }
+
+    // check duplicates
+    // TODO necessary? better: check server-side when link is sent → react on response error
+    socket.emit('check_duplicate', {slug: localStorage.slug, url: htmlEntities(url)});
+
+    // add link
+    // TODO badge on success
+    socket.emit('add', {slug: localStorage.slug, title: title, url: url});
+  });
 });
-
-
-
