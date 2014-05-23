88b4a34e4cdffd65e97ca46a1470cb3cec0c3048
Enrico Schlag <enricoschlag@googlemail.com>
2014-05-23 16:45:29 +0200

    minor fixes for time formatting

--- server/public/js/live.js
+++ server/public/js/live.js
@@ -1,23 +1,29 @@
 /*global io */
-/*global console */
 
 // TODO title-edits and deletes
 // TODO on delete: check if we have more than one link left, if false: show alert-info again
 
+// vars and functions
+// -----------------------------------------------------------------------------
+
 var socket = io.connect('http://localhost:63685'),
 
-padZero = function(num) {
-  return num < 10 ? "0" + num : num;
-},
-// item.time-start+offset
-formatTime = function(milliseconds) {
-  var seconds = Math.floor(milliseconds / 1000),
-      hours   = Math.floor(seconds / 3600),
-      minutes = Math.floor((seconds / 60) % 60);
-  return padZero(hours % 24) + ':' + padZero(minutes) + ':' + padZero(seconds % 60);
-},
+    padZero = function(num) {
+      return num < 10 ? "0" + num : num;
+    },
 
-tzOffset = new Date().getTimezoneOffset() * 60000;  // Different between UTC and local time;
+    formatTime = function(milliseconds) {
+      var seconds = Math.floor(milliseconds / 1000),
+          hours   = Math.floor(seconds / 3600),
+          minutes = Math.floor((seconds / 60) % 60);
+      return padZero(hours % 24) + ':' + padZero(minutes) + ':' + padZero(seconds % 60);
+    },
+
+    tzOffset = new Date().getTimezoneOffset() * 60000;  // Different between UTC and local time;
+
+
+// socket bindings
+// -----------------------------------------------------------------------------
 
 socket.on('connect', function () {
   socket.emit('live', $('body').data('publicslug'));
@@ -27,7 +33,7 @@ socket.on('push', function(data) {
   if ($('#auto').is(':checked') && !data.isText)
     window.open(data.url, '_blank');
   
-  var markup = '<span class="time">' + formatTime (data.time - tzOffset) + '</span>';
+  var markup = '<span class="time">' + formatTime(data.time - tzOffset) + '</span>';
   if (data.isText)
     markup += data.title;
   else
@@ -41,6 +47,7 @@ socket.on('counter', function(data) {
   $('#counter').html(data);
 });
 
+// TODO what for?
 socket.on('reload', function() {
   location.reload();
 });
@@ -49,6 +56,12 @@ socket.on('titleUpdatedSuccess', function(data) {
   $('#title').text(data.title);
 });
 
-$('.time').each(function(){
-  $(this).text(formatTime($(this).text()-tzOffset));
+
+// init
+// -----------------------------------------------------------------------------
+
+// format time from timestamp
+$('.time').each(function() {
+  var $this = $(this);
+  $this.text(formatTime(parseInt($this.closest('li').data('time')) - tzOffset));
 });
