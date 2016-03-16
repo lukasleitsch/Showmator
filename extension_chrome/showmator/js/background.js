f3da42f1eb3dd8cd3087a399d4997d8a69a5e38c
Christian Blume <chblume86@gmail.com>
2016-03-16 09:35:27 +0100

    Modularise `background.js`
    The content of `background.js` has been modularised.

--- extension_chrome/showmator/js/background.js
+++ extension_chrome/showmator/js/background.js
@@ -1,27 +1,62 @@
 /*global chrome */
 
+/*global $ */
 
-var shortcut = false;
+var Background = (function() {
+
+  var module = {},
+
+    
+  // Private
+  // ---------------------------------------------------------------------
+
+    // Variables
+    // ---------------------------------------------------------------------
+
+    _shortcut = '',
 
 
-// fetches shortcut (can't be done inside message-listener because runtime ports
-// get invalid somehow)
-chrome.commands.getAll(function(commands) {
-  $.each(commands, function(key, val) {
-    if (val.name == '_execute_browser_action' && val.shortcut !== '')
-      shortcut = val.shortcut;
-  });
-});
+    // Handlers
+    // ---------------------------------------------------------------------
+
+    // delivers slug from extension to content script and opens popup
+    _handleMessageEvent = function(request, sender, sendResponse) {
+      if (request.type === 'showmatorAdminRequestFromScript' && request.publicSlug === localStorage.publicSlug) {
+        sendResponse({
+          slug:     localStorage.slug,
+          shortcut: _shortcut
+        });
+      }
+      else if (request.type === 'showmatorConfigureShortcut') {
+        chrome.tabs.create({url: 'chrome://extensions/configureCommands'});
+      }
+    },
 
 
-// delivers slug from extension to content script and opens popup
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.type == 'showmatorAdminRequestFromScript' && request.publicSlug == localStorage.publicSlug)
-    sendResponse({
-      slug:     localStorage.slug,
-      shortcut: shortcut
-    });
+    // Helper functions
+    // ---------------------------------------------------------------------
 
-  else if (request.type == 'showmatorConfigureShortcut')
-    chrome.tabs.create({url: 'chrome://extensions/configureCommands'});
-});
+    _fetchShortcut = function(commands) {
+      $.each(commands, function(key, val) {
+        if (val.name === '_execute_browser_action' && val.shortcut !== '') {
+          _shortcut = val.shortcut;
+        }
+      });
+    },
+
+    _setUp = function() {
+      chrome.commands.getAll(_fetchShortcut);
+      chrome.runtime.onMessage.addListener(_handleMessageEvent);
+    };
+
+  // Public
+  // ---------------------------------------------------------------------
+
+  module.init = function() {
+    _setUp();
+  };
+
+  return module;
+})();
+
+Background.init();
